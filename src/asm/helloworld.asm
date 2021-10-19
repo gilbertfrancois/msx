71cc9ed7080998761fc69de47b2115a21c32f9eb
Gilbert Francois Duivesteijn <info@gilbertfrancois.com>
2021-10-20 01:30:10 +0200

    Converts tabs to spaces

--- src/asm/helloworld.asm
+++ src/asm/helloworld.asm
@@ -8,19 +8,19 @@ CHPUT   equ &00A2
 LINLEN  equ &F3B0
 
 ; Compilation address
-	org &4000
+    org &4000
 
 ; ROM header
-	db "AB"     ; magic number
-	dw Execute  ; program execution address
-	dw 0, 0, 0, 0, 0, 0
+    db "AB"     ; magic number
+    dw Execute  ; program execution address
+    dw 0, 0, 0, 0, 0, 0
 
 ; Program code entry point
 Execute:
-	call INIT32        ; set screen 1
+    call INIT32        ; set screen 1
     ld a, 32
     call LINLEN        ; set width 32
-	ld hl,helloWorld   ; load string
+    ld hl,helloWorld   ; load string
     call PrintString   ; print string
     call NewLine       ; goto new line
     call Finished      ; end
@@ -45,13 +45,13 @@ NewLine:
 ; Halt program execution. Change to "ret" to return to MSX-BASIC.
 Finished:
     ; ret
-	di
-	halt
+    di
+    halt
 
 ; Data
 helloWorld:
-	db "Hello world!",0
+    db "Hello world!",0
 
 ; Padding to make the file size a multiple of 16K
 ; (Alternatively, include macros.asm and use ALIGN 4000H)
-	ds -$ & &3FFF
+    ds -$ & &3FFF
