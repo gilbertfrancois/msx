fc73220a603bf4597d87d73f6072829925efb654
Gilbert Francois Duivesteijn <info@gilbertfrancois.com>
2021-11-08 10:13:56 +0100

    Updates build scripts

---
+++ bin/build_glass.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+PROJECT_ROOT=${DIR}/..
+
+rm -f ${PROJECT_ROOT}/build/*
+
+java -jar ${PROJECT_ROOT}/bin/glass.jar $1 ${PROJECT_ROOT}/build/out.${2}
+
+# ${PROJECT_ROOT}/bin/run_rom.sh
