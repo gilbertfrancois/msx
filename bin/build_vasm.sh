92271379a08acadb917c07d9c58a599b358b10a5
Gilbert Francois Duivesteijn <info@gilbertfrancois.com>
2021-11-08 17:05:32 +0100

    Updates vasm build script

--- bin/build_vasm.sh
+++ bin/build_vasm.sh
@@ -6,8 +6,9 @@ PROJECT_ROOT=${DIR}/..
 
 rm -f ${PROJECT_ROOT}/build/*
 
-vasmz80_oldstyle ${1} -chklabels -nocase -Dvasm=1 -Fbin \
+vasmz80_oldstyle ${1} -chklabels -nocase -Dvasm=1 -DBuildMSX=1 -DBuildMSX_MSX1=1 \
     -L ${PROJECT_ROOT}/build/listing.txt \
+    -Fbin \
     -o ${PROJECT_ROOT}/build/out.${2}
 
 # ${PROJECT_ROOT}/bin/run_rom.sh
