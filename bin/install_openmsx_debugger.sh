97a4170c7c5b5c5767a0e43888e8408e992b2739
Gilbert Francois Duivesteijn <gilbert@deep-impact.ch>
2021-11-08 11:12:25 +0100

    Install script for opendebugger

---
+++ bin/install_openmsx_debugger.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+brew install qt@5
+
+
+export LDFLAGS="-L/usr/local/opt/qt@5/lib"
+export CPPFLAGS="-I/usr/local/opt/qt@5/include"
+export PKG_CONFIG_PATH="/usr/local/opt/qt@5/lib/pkgconfig"
+export PATH="/usr/local/opt/qt@5/bin:$PATH"
+
+cd /tmp
+git clone https://github.com/openMSX/debugger.git
+cd debugger
+
+make
+sudo cp -r ./derived/openMSX_Debugger.app /Applications/
