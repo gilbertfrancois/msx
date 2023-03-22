84789882c32009383c85c242ed6ec504cbd7d6d6
Gilbert Francois Duivesteijn <gilbert@deep-impact.ch>
2023-03-22 18:36:37 +0100

    Updates compile script for openMSX debugger.

--- bin/install_openmsx_debugger.sh
+++ bin/install_openmsx_debugger.sh
@@ -3,10 +3,14 @@
 brew install qt@5
 
 
-export LDFLAGS="-L/usr/local/opt/qt@5/lib"
-export CPPFLAGS="-I/usr/local/opt/qt@5/include"
-export PKG_CONFIG_PATH="/usr/local/opt/qt@5/lib/pkgconfig"
-export PATH="/usr/local/opt/qt@5/bin:$PATH"
+# export LDFLAGS="-L/usr/local/opt/qt@5/lib"
+# export CPPFLAGS="-I/usr/local/opt/qt@5/include"
+# export PKG_CONFIG_PATH="/usr/local/opt/qt@5/lib/pkgconfig"
+# export PATH="/usr/local/opt/qt@5/bin:$PATH"
+export PATH="/opt/homebrew/Cellar/qt@5/5.15.6/bin:$PATH"
+export PKG_CONFIG_PATH="/opt/homebrew/Cellar/qt@5/5.15.6/lib/pkgconfig"
+export CPPFLAGS="-I/opt/homebrew/Cellar/qt@5/5.15.6/include"
+export LDFLAGS-"-L/opt/homebrew/Cellar/qt@5/5.15.6/lib"
 
 cd /tmp
 git clone https://github.com/openMSX/debugger.git
