402de9cbb9557c777543c3b6ec9022133aed651d
Gilbert Francois Duivesteijn <info@gilbertfrancois.com>
2023-07-18 09:59:11 +0200

    fp (WIP)

--- src/asm/08_floatingpoint/fp.bin.asm
+++ src/asm/08_floatingpoint/fp.bin.asm
@@ -8,7 +8,7 @@ SETWRT      equ $0053
 VDPData     equ $98
 VDPControl  equ $99
 
-FRAC	    equ 8
+FRAC        equ 8
 FRACV       equ 255
 ROUNDV      equ 127
 
@@ -39,6 +39,40 @@ FileStart:
 
 Main:
     
+    ; calc Xmin, Xmax
+    ld hl, (SizeWidth)
+    ld de, FP2 
+    call _f16_div_hl_de
+    ld de, (Xc)
+    ex de, hl
+    call _f16_sub_hl_de
+    ld (Xmin), hl
+    ld de, (SizeWidth)
+    call _f16_add_hl_de
+    ld (Xmax), hl
+    ; ld hl, (Xmin)
+    ; ld ($9000), hl
+    ; ld hl, (Xmax)
+    ; ld ($9002), hl
+
+    ; calc Ymin, Ymax
+    ld hl, (SizeHeight)
+    ld de, FP2 
+    call _f16_div_hl_de
+    ld de, (Yc)
+    ex de, hl
+    call _f16_sub_hl_de
+    ld (Ymin), hl
+    ld de, (SizeHeight)
+    call _f16_add_hl_de
+    ld (Ymax), hl
+    ld hl, (Ymin)
+    ld ($9000), hl
+    ld hl, (Ymax)
+    ld ($9002), hl
+
+    ret
+
     ; call CHGET
 
 ; for ty=0 to 24
@@ -78,29 +112,23 @@ Main:
     ; call CHGET
     ; ret
 
+    ; ld hl, &4248
+    ; call _f16_int_hl
 
-    ld hl, 25
-    ld de, 21
-    call _f16_from_int_dehl
-    ld ($9000), hl
+    ; call _f16_from_int_dehl
 
-    ld hl, $4248 ; Pi
-    call _f16_int_hl
-    ld ($9002), hl
-    
-    ret
-
-    ; Change to screen 0
-    ; ld a, 0
-    ; call CHGMOD
-    ; ld hl, $4248 ; 72
-    ; ld de, $4500 ; 128
-    ; call _f16_mul_hl_de
+    ; ld hl, &0001
+    ; call _f16_from_int_dehl
     ; ld ($9000), hl
-    call FunctionZ
-    ld hl, (X)
-    ld ($9000), hl
-    ret
+
+    ; ld hl, &0002
+    ; call _f16_from_int_dehl
+    ; ld ($9002), hl
+
+    ; ld hl, &0072
+    ; call _f16_from_int_dehl
+    ; ld ($9004), hl
+
 
 
 FunctionTx:
@@ -123,39 +151,39 @@ FunctionZ:
 ; 620 NEXT D
 ; 540 X=0
     ld hl, 0
-    ld (X), hl
+    ld (zx), hl
 ; 550 Y=0
-    ld (Y), hl
+    ld (zy), hl
 ; 560 FOR D=0 TO DE-1
     ld b, $0f
 FunctionZLoop:
     push bc
 ; 570 XN=X*X-Y*Y+CX
-    ld hl, (X)
+    ld hl, (zx)
     call _f16_pow2_hl    ; X*X
-    ld (Xn), hl          ; Xn := X*X
-    ld hl, (Y)
+    ld (znx), hl          ; Xn := X*X
+    ld hl, (zy)
     call _f16_pow2_hl    ; Y*Y 
-    ld de, (Xn)
+    ld de, (znx)
     call _f16_sub_hl_de  ; X*X - Y*Y
-    ld de, (Cx)
+    ld de, (cx)
     call _f16_add_hl_de  ; X*X - Y*Y + Cx
-    ld (Xn), hl          ; Xn := X*X - Y*Y + Cx
+    ld (znx), hl          ; Xn := X*X - Y*Y + Cx
 ; 580 YN=2*X*Y+CY
     ld hl, FP2           ; 2
-    ld de, (X)           ; X
+    ld de, (zx)           ; X
     call _f16_mul_hl_de  ; 2*X
-    ld de, (Y)           ; Y
+    ld de, (zy)           ; Y
     call _f16_mul_hl_de  ; 2*X*Y
-    ld de, (Cy)
+    ld de, (cy)
     call _f16_add_hl_de  ; 2*X*Y + Cy
-    ld (Yn), hl          ; Yn := 2*X*Y + Cy
+    ld (zny), hl          ; Yn := 2*X*Y + Cy
 ; 590 IF (XN*XN+YN*YN) > 4 THEN RETURN
-    ld hl, (Xn)
+    ld hl, (znx)
     call _f16_pow2_hl    ; XN*XN
     ld d, h
     ld e, l
-    ld hl, (Yn)
+    ld hl, (zny)
     call _f16_pow2_hl    ; YN*YN
     call _f16_add_hl_de  ; XN*XN + YN*YN
     ld de, FP4           ; 4
@@ -163,11 +191,11 @@ FunctionZLoop:
     ld a, l
     jr nz, FunctionZEnd
 ; 600 X=XN
-    ld hl, (Xn)
-    ld (X), hl
+    ld hl, (znx)
+    ld (zx), hl
 ; 610 Y=YN
-    ld hl, (Yn)
-    ld (Y), hl
+    ld hl, (zny)
+    ld (zy), hl
 ; 620 NEXT D
     ld a, (Depth)
     inc a
@@ -190,25 +218,36 @@ Xc:
     db $00, $b8
 Yc:
     db $0d, $b9
-Size:
+SizeWidth:
+    db $00, $42
+SizeHeight:
     db $00, $40
+Xmin: 
+    db $00, $00
+Xmax:
+    db $00, $00
+Ymin: 
+    db $00, $00
+Ymax: 
+    db $00, $00
+
 Stride:
     db $00
 ColorMode:
     db $00 
 
 ; VARIABLES
-X: 
+zx: 
     db $00, $00
-Y:
+zy:
     db $00, $00
-Xn:
+znx:
     db $00, $00
-Yn:
+zny:
     db $00, $00
-Cx: 
+cx: 
     db $00, $3c ; 1
-Cy:
+cy:
     db $00, $40 ; 2
 Depth:
     db $00
