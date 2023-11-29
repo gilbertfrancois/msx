08a0bc8043df72031b45c7b77fa0fd5e22d5f697
Gilbert Francois Duivesteijn <gilbert@deep-impact.ch>
2023-11-29 22:39:44 +0100

    Bug fix

--- src/asm/05_interrupts/interrupts.bin.asm
+++ src/asm/05_interrupts/interrupts.bin.asm
@@ -7,60 +7,61 @@
 ORGADR      equ $c000
 BEEP        equ $00c0
 HTIMI       equ $fd9f
-MaxCount    equ 50
+MAX_COUNT   equ 50
 
     ; Place header before the binary.
     org ORGADR - 7
     ; Bin header, 7 bytes
     db $fe
-    dw FileStart
-    dw FileEnd - 1
-    dw Main
+    dw _file_start
+    dw _file_end - 1
+    dw _main
 
     ; org statement after the header
     org ORGADR
 
-FileStart:
-Main:
+_file_start:
+_main:
     ; Install hook, run once
     di
     ; Preserve old hook instructions
-    ld de, OldHook
     ld hl, HTIMI
+    ld de, _old_hook
     ld bc, 5
     ldir
     ; Copy new hook instructions
-    ld hl, NewHook
+    ld hl, _new_hook
     ld de, HTIMI
     ld bc, 5
     ldir
-
     ei
-
     ; Return to Basic
     ret
 
-NewHook:
-    jp BeepFn
+_new_hook:
+    jp _on_interrupt
     ret
     nop
 
-OldHook:
-    ; Reserve 5 bytes to store the old hook
-    db 0, 0, 0, 0, 0
-
-BeepFn:
+_on_interrupt:
     ; Run at every interrupt
-    ld hl, Counter
+    ld hl, _counter
     dec (hl)
     ld a, (hl)
-    jp nz, OldHook
-    ; Reset counter and call BEEP
-    ld (hl), MaxCount
+    jp nz, _old_hook
+    ; Run once every MAX_COUNT times
+    ; Reset _counter and call BEEP
+    ld (hl), MAX_COUNT
     call BEEP
+_old_hook:
+    ; Place old hook here, so it will be called directly after
+    ; the new on_interrupt function.
+    ; Reserve 5 bytes to store the old hook
+    ds 5, 0
+    ret
 
-Counter:
-    db MaxCount
+_counter:
+    db MAX_COUNT
 
-FileEnd:
+_file_end:
 
